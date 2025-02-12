60e8a1ec027288ea695775ae15f34761f1bbbb85
Aiden Mayoros <aiden.mayoros@gmail.com>
2025-02-12 15:15:29 -0700

    game now running at max 60fps

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = game_clock.tick(60)
 
 
 if __name__ == "__main__":
