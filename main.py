c80c23e8422bcf09604bf2b8c26affbd55415dda
Aiden Mayoros <aiden.mayoros@gmail.com>
2025-02-13 15:56:43 -0700

    Player triangle is showing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +20,7 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)
 
