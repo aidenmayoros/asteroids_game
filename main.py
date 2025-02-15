7a0dd4b688fd345b0d11e6d724696317cf79fa44
Aiden Mayoros <aiden.mayoros@gmail.com>
2025-02-15 10:42:36 -0700

    starting collision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -15,10 +17,16 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     updatable.add(player)
     drawable.add(player)
+    AsteroidField()
 
     while True:
         for event in pygame.event.get():
